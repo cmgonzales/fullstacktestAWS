2f514315989386134ab22e9daf45a33962ddd5e6
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-08-11 18:13:30 -0700

    test

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const port = process.env.PORT || "5500";
+const path = require('path')
+
+const app = express()
+//dd
+
+app.use(express.static(path.join(__dirname, '/build/test/')));
+
+app.get('/test', (req, res) => {
+    let list = ["monkey", "cow", "fufuhead"]
+ res.send(list)
+})
+
+ app.get('*', (req,res) =>{
+	res.sendFile(path.join(__dirname+'/test/build/index.html'));
+ });
+
+app.listen(port, () => console.log("listening"));
